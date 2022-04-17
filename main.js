76a4fb1900109aad4cf332372ad7f394dce56143
GiorgosTsakonas <giorgos_tsakonas@outlook.com>
2022-04-18 02:50:55 +0300

    Not great, not terrible

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 let cells = document.querySelectorAll("[data-cell]");
 
+//players names
+var btn = document.querySelector("button");
+var firstPlayer = [];
+var secondPlayer = [];
+
+btn.addEventListener("click", function(){
+  var player1 = document.getElementById( "firstPlayer");
+  var player2 = document.getElementById("secondPlayer");
+  let singleName = true;
+
+  if (player1.value != " " && player2.value != " ") {
+   firstPlayer.push(player1.value);
+    secondPlayer.push(player2.value);
+    alert ("First players name is: " + firstPlayer);
+    alert ("Second players name is: " +secondPlayer);
+    const name = document.getElementsByClassName("name");
+name[0].innerHTML = firstPlayer;
+name[1].innerHTML = secondPlayer;
+  } 
+  else {
+  	alert("Enter players' names");
+  }	
+});
+
+
+
+
 // this will give each element an event listener
 cells.forEach((cell, i) => {
   cell.addEventListener("click", handleClick, { once: true });
@@ -13,14 +40,16 @@ function handleClick(e) {
   counter++;
   let cell = e.target;
   if (choice) {
-    cell.style.backgroundImage = "url('/images/x.png')";
-    cell.style.color = "red";
-  } else {
     cell.style.backgroundImage = "url('/images/o.png')";
     cell.style.color = "green";
+
+    
+  } else {
+    cell.style.backgroundImage = "url('/images/x.png')";
+    cell.style.color = "red";
   }
-  option("red");
   option("green");
+  option("red");
   choice = !choice;
 }
 
